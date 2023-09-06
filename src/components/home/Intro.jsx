4894a7100bef33218d189ca05cc7601cc713bfde
VCarames <109115310+VCarames@users.noreply.github.com>
2023-09-06 13:31:50 -0700

    Home Intro Section + Button Component
    Home "Intro" section initial styling; Hover and focus effects pending.
    
    "Button" component created.

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -4,10 +4,10 @@ function Intro() {
   return (
     <section className="intro-section">
       <div className="intro-section__content">
-        <h1 className="intro-section__heading">
+        <h1 className="intro-section__heading intro__heading">
           Award-winning custom designs and digital branding solutions
         </h1>
-        <p className="intro-section__text">
+        <p className="intro-section__text intro__text">
           With over 10 years in the industry, we are experienced in creating
           fully responsive websites, app design, and engaging brand experiences.
           Find out more about our services.
