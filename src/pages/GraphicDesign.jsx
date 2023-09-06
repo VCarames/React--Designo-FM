5e897b7e076e965cfd6dd0ca3f60b7f2343cfed3
VCarames <109115310+VCarames@users.noreply.github.com>
2023-09-06 10:57:32 -0700

    Graphic Design Page + JSON File
    "Graphic Design" page content created; "Graphic Project Section"
    
    Section data is imported via JSON file, "graphicProjects".

--- src/pages/GraphicDesign.jsx
+++ src/pages/GraphicDesign.jsx
@@ -3,19 +3,48 @@ import Projects from "../components/Projects";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer/Footer";
 import Intro from "../components/Intro";
+import graphicProjectsData from "/public/data/graphicProjects";
 
 function GraphicDesign() {
   return (
     <div>
       <Header />
-      <Intro
-        heading="Graphic Design"
-        text="We deliver eye-catching branding materials that are 
+
+      <main>
+        <Intro
+          heading="Graphic Design"
+          text="We deliver eye-catching branding materials that are 
         tailored to meet your business objectives.
         "
-      />
-      Graphic Design Page
-      <main>
+        />
+
+        <section className="graphic-projects-section">
+          <h2 className="visually-hidden">Our Projects</h2>
+          <div className="graphic-projects-section__content">
+            <ul className="graphic-projects-section__list" role="list">
+              {graphicProjectsData.graphicProjects.map((graphicProject) => (
+                <li
+                  className="graphic-projects-section__item"
+                  key={graphicProject.id}
+                  role="listitem"
+                >
+                  <img
+                    className="graphic-projects-section__image"
+                    src={graphicProject.image}
+                    alt=""
+                  />
+                  <h3 className="graphic-projects-section__subheading">
+                    {graphicProject.heading}
+                  </h3>
+                  <p className="graphic-projects-section__text">
+                    {graphicProject.text}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
         <Projects />
         <CTA />
       </main>
