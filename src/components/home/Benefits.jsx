363c480ca88d845f0d2a1b6857f1e24fd205463b
VCarames <109115310+VCarames@users.noreply.github.com>
2023-09-07 01:29:39 -0700

    Benefits Component - Styling
    Initial styling for "Mobile", "Tablet" and "Desktop"; Hover and focus effects pending.

--- src/components/home/Benefits.jsx
+++ src/components/home/Benefits.jsx
@@ -9,12 +9,16 @@ function Benefits() {
       <ul className="benefits-section__list" role="list">
         <li className="benefits-section__item" role="listitem">
           <img className="benefits-section__icon" src={PassionateIcon} alt="" />
-          <h3 className="benefits-section__subheading">Passionate</h3>
-          <p className="benefits-section__text">
-            Each project starts with an in-depth brand research to ensure we
-            only create products that serve a purpose. We merge art, design, and
-            technology into exciting new solutions.
-          </p>
+          <div className="benefits-section__container">
+            <h3 className="benefits-section__subheading subheading--20 ">
+              Passionate
+            </h3>
+            <p className="benefits-section__text">
+              Each project starts with an in-depth brand research to ensure we
+              only create products that serve a purpose. We merge art, design,
+              and technology into exciting new solutions.
+            </p>
+          </div>
         </li>
         <li className="benefits-section__item" role="listitem">
           <img
@@ -22,21 +26,29 @@ function Benefits() {
             src={ResourcefulIcon}
             alt=""
           />
-          <h3 className="benefits-section__subheading">Resourceful</h3>
-          <p className="benefits-section__text">
-            Everything that we do has a strategic purpose. We use an agile
-            approach in all of our projects and value customer collaboration. It
-            guarantees superior results that fulfill our clients’ needs.
-          </p>
+          <div className="benefits-section__container">
+            <h3 className="benefits-section__subheading subheading--20 ">
+              Resourceful
+            </h3>
+            <p className="benefits-section__text">
+              Everything that we do has a strategic purpose. We use an agile
+              approach in all of our projects and value customer collaboration.
+              It guarantees superior results that fulfill our clients’ needs.
+            </p>
+          </div>
         </li>
         <li className="benefits-section__item" role="listitem">
           <img className="benefits-section__icon" src={FriendlyIcon} alt="" />
-          <h3 className="benefits-section__subheading">Friendly</h3>
-          <p className="benefits-section__text">
-            We are a group of enthusiastic folks who know how to put people
-            first. Our success depends on our customers, and we strive to give
-            them the best experience a company can provide.
-          </p>
+          <div className="benefits-section__container">
+            <h3 className="benefits-section__subheading subheading--20 ">
+              Friendly
+            </h3>
+            <p className="benefits-section__text">
+              We are a group of enthusiastic folks who know how to put people
+              first. Our success depends on our customers, and we strive to give
+              them the best experience a company can provide.
+            </p>
+          </div>
         </li>
       </ul>
     </section>
