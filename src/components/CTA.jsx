174d4272998dfa3ddb03e2a2be1ceb09d2a8c9ab
VCarames <109115310+VCarames@users.noreply.github.com>
2023-09-07 22:07:44 -0700

    CTA - Styling
    Initial styling for "Mobile", "Tablet" and "Desktop"; Hover and focus effects pending.

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,12 +4,16 @@ function CTA() {
   return (
     <section className="cta-section">
       <div className="cta-section__content">
-        <h2 className="cta-section__heading">Let’s talk about your project</h2>
-        <p className="cta-section__text">
-          Ready to take it to the next level? Contact us today and find out how
-          our expertise can help your business grow.
-        </p>
-        <Link className="cta-section__btn" to="/contact">
+        <div className="cta-section__container">
+          <h2 className="cta-section__heading primary-heading">
+            Let’s talk about your project
+          </h2>
+          <p className="cta-section__text primary-text">
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
+        <Link className="cta-section__btn btn" to="/contact">
           Get In Touch
         </Link>
       </div>
