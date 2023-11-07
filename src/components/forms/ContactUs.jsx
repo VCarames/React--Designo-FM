23a30ce99af4a97d4f9cf50e59a5f6fd81d03ca0
VCarames <109115310+VCarames@users.noreply.github.com>
2023-11-07 13:29:02 -0800

    Contact Us Form Functions Added

--- src/components/forms/ContactUs.jsx
+++ src/components/forms/ContactUs.jsx
@@ -1,4 +1,56 @@
+import React, { useState } from "react";
+
 function ContactUs() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePhone = (phone) => {
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+    return phoneRegex.test(phone);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = {};
+
+    if (formData.name.trim() === "") {
+      newErrors.name = "Name is required";
+    }
+
+    if (!validateEmail(formData.email)) {
+      newErrors.email = "Invalid email address";
+    }
+
+    if (!validatePhone(formData.phone)) {
+      newErrors.phone = "Invalid phone number";
+    }
+
+    if (formData.message.trim() === "") {
+      newErrors.message = "Message is required";
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <section className="contact-section">
       <div className="contact-section__content">
@@ -9,11 +61,15 @@ function ContactUs() {
           <p className="contact-section__text text--primary">
             Ready to take it to the next level? Let’s talk about your project or
             idea and find out how we can help your business grow. If you are
-            looking for unique digital experiences that’s relatable to your
+            looking for unique digital experiences that are relatable to your
             users, drop us a line.
           </p>
         </div>
-        <form className="contact-section__form" noValidate>
+        <form
+          className="contact-section__form"
+          noValidate
+          onSubmit={handleSubmit}
+        >
           <fieldset className="contact-section__form-fieldset">
             <label className="visually-hidden" htmlFor="name">
               Name
@@ -23,8 +79,14 @@ function ContactUs() {
               type="text"
               name="name"
               id="name"
+              aria-describedby="email-error"
               placeholder="Name"
+              value={formData.name}
+              onChange={handleInputChange}
             />
+            {errors.name && (
+              <p className="contact-section__form-error">{errors.name}</p>
+            )}
 
             <label className="visually-hidden" htmlFor="email">
               Email Address
@@ -35,7 +97,12 @@ function ContactUs() {
               name="email"
               id="email"
               placeholder="Email Address"
+              value={formData.email}
+              onChange={handleInputChange}
             />
+            {errors.email && (
+              <p className="contact-section__form-error">{errors.email}</p>
+            )}
 
             <label className="visually-hidden" htmlFor="phone">
               Phone
@@ -47,7 +114,12 @@ function ContactUs() {
               id="phone"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               placeholder="Phone"
+              value={formData.phone}
+              onChange={handleInputChange}
             />
+            {errors.phone && (
+              <p className="contact-section__form-error">{errors.phone}</p>
+            )}
 
             <label className="visually-hidden" htmlFor="message">
               Your Message
@@ -58,7 +130,12 @@ function ContactUs() {
               name="message"
               id="message"
               placeholder="Your Message"
+              value={formData.message}
+              onChange={handleInputChange}
             />
+            {errors.message && (
+              <p className="contact-section__form-error">{errors.message}</p>
+            )}
           </fieldset>
 
           <button
